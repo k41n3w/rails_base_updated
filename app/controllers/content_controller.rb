ee93707b0da2e39c28f5969b3923fdcceee52249
Caio Ramos <kaineo@hotmail.com>
2021-02-17 13:47:33 -0300

    Test credentials on deploy

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -6,6 +6,7 @@ class ContentController < ApplicationController
       rails_env: Rails.env,
       ruby_version: '3.0.0',
       rails_version: '6.1.1',
+      credentials_key: Rails.application.credentials[Rails.env.to_sym][:SOME_KEY],
       developed_by: 'k41n3w'
     }, status: :ok
   end
--- spec/requests/content_request_spec.rb
+++ spec/requests/content_request_spec.rb
@@ -6,7 +6,13 @@ RSpec.describe 'Contents', type: :request do
   describe 'GET /show' do
     it 'returns http success' do
       get '/content/show'
+      json = JSON(response.body)
+
       expect(response).to have_http_status(:success)
+      expect(json['ruby_version']).to eq('3.0.0')
+      expect(json['rails_version']).to eq('6.1.1')
+      expect(json['credentials_key']).to eq(Rails.application.credentials[Rails.env.to_sym][:SOME_KEY])
+      expect(json['developed_by']).to eq('k41n3w')
     end
   end
 end
