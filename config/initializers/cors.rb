77a3b7f026eb32546dc8714a03855fa2ea56372f
Caio Ramos <kaineo@hotmail.com>
2021-03-29 11:03:47 -0300

    Add localhost:8080 to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'example.com'
+    origins 'http://localhost:8080'
 
     resource '*',
       headers: :any,
